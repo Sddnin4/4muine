3adf393042ddf9a13c9e6e4591258b63122bba8c
Mila <builonggiaduc08032010@gmail.com>
2026-06-02 21:47:53 +0700

    Refactor TrongTrotListener by removing comments
    Remove comments related to growth stages in spring.

--- TrongTrotListener.java
+++ TrongTrotListener.java
@@ -14,14 +14,11 @@ public class TrongTrotListener implements Listener {
         long tongSoNgay = world.getFullTime() / 24000;
         long ngayTrongNam = tongSoNgay % 360;
 
-        // Nếu đang nằm trong phạm vi 90 ngày của Mùa Xuân
         if (ngayTrongNam < 90) {
-            // Ép kiểu sang thuộc tính tăng trưởng theo thời gian của block (1.21 chuẩn)
             if (event.getNewState().getBlockData() instanceof Ageable ageable) {
                 int maxAge = ageable.getMaximumAge();
                 int currentAge = ageable.getAge();
                 
-                // Nếu cây chưa chín hoàn toàn, cộng thêm 1 giai đoạn lớn cho nó
                 if (currentAge < maxAge) {
                     ageable.setAge(Math.min(maxAge, currentAge + 1));
                     event.getNewState().setBlockData(ageable);
